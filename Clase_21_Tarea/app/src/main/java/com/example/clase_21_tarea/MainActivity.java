edde00ad35b99ce135b1f3321a85e2cf360117d3
Juan Garcia Montalvillo <montalvillo@outlook.com>
2019-01-21 12:17:24 +0100

    terminada

---
+++ Clase_21_Tarea/app/src/main/java/com/example/clase_21_tarea/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.clase_21_tarea;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        añadirFragment();
+    }
+
+    public void añadirFragment(){
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        PrimerFragment fragment = new PrimerFragment();
+        fragmentTransaction.add(R.id.fragmentoUno,fragment);
+        fragmentTransaction.commit();
+
+
+    }
+}
